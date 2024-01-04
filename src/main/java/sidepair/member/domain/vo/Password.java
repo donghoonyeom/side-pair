3aac4a97f2416e63144e7a4f602537e0437ed9ee
donghoonyeom <dua5410773@naver.com>
2024-01-04 22:39:16 +0900

    feat(domain): Password 및 EncryptedPassword 클래스 추가
    Password 클래스는 비밀번호를 나타내는 값 객체이며, 주어진 규칙에 따라 비밀번호의 유효성을 확인합니다.
    - 최소 길이: 8
    - 최대 길이: 15
    - 정규 표현식: "^(?=.*[a-z])(?=.*\\d)[a-z\\d!@#\\$%\\^&\\*\\(\\)~]+$"
    
    EncryptedPassword 클래스는 Password를 암호화하고, 랜덤한 salt를 생성하여 저장합니다.
    - 암호화 알고리즘: SHA-256
    - 비밀번호와 salt를 조합하여 해싱
    
    두 클래스는 회원 비밀번호와 암호화된 비밀번호를 효과적으로 다룰 수 있도록 설계되었습니다.
    
    추가 정보:
    - Password 클래스: 비밀번호 유효성 검사 및 값 객체
    - EncryptedPassword 클래스: 비밀번호 암호화 및 비교 기능

---
+++ src/main/java/sidepair/member/domain/vo/Password.java
@@ -0,0 +1,40 @@
+package sidepair.member.domain.vo;
+
+import sidepair.member.exception.MemberException;
+
+public class Password {
+
+    private static final int MIN_LENGTH = 8;
+    private static final int MAX_LENGTH = 15;
+    private static final String REGEX = "^(?=.*[a-z])(?=.*\\d)[a-z\\d!@#\\$%\\^&\\*\\(\\)~]+$";
+
+    private final String value;
+
+    public Password(final String value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(final String value) {
+        if (isNotValidLength(value) || isNotValidPattern(value)) {
+            throw new MemberException.PasswordRegexException(value);
+        }
+    }
+
+    private boolean isNotValidLength(final String value) {
+        return value.length() < MIN_LENGTH || value.length() > MAX_LENGTH;
+    }
+
+    private boolean isNotValidPattern(final String value) {
+        return !value.matches(REGEX);
+    }
+
+    public int length() {
+        return value.length();
+    }
+
+    public byte[] getBytes() {
+        return value.getBytes();
+    }
+}
+
