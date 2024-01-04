d69cf5d24fdf096dd8d2bfdcaeeab094bae56700
donghoonyeom <dua5410773@naver.com>
2024-01-04 22:35:45 +0900

    refactor(member): Skill 열거형 리팩토링
    Skill 열거형을 리팩토링하여 입력값의 유효성을 검사하는 로직을 개선했습니다.

--- src/main/java/sidepair/member/domain/vo/Skill.java
+++ src/main/java/sidepair/member/domain/vo/Skill.java
@@ -1,5 +1,6 @@
 package sidepair.member.domain.vo;
 
+import java.util.Arrays;
 import lombok.Getter;
 
 @Getter
@@ -22,9 +23,29 @@ public enum Skill {
 
     private final String displayName;
 
-    Skill(String displayName) {
-        this.displayName = displayName;
+    Skill(String inputSkill) {
+        validateNull(inputSkill);
+        validateBlank(inputSkill);
+        this.displayName = inputSkill.trim();
+        validateExistence(this.displayName);
     }
 
+    private void validateNull(String displayName) {
+        if (displayName == null) {
+            throw new NullPointerException("기술 이름은 null일 수 없습니다.");
+        }
+    }
 
+    private void validateBlank(String displayName) {
+        if (displayName.isBlank()) {
+            throw new IllegalArgumentException("기술 이름은 공백일 수 없습니다.");
+        }
+    }
+
+    private void validateExistence(String displayName) {
+        if (Arrays.stream(values())
+                .noneMatch(skill -> skill.displayName.equals(displayName))) {
+            throw new IllegalArgumentException("존재하지 않는 기술입니다.");
+        }
+    }
 }
