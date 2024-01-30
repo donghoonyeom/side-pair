8767eae5aecc0a13beed1076e323531c60ff192b
donghoonyeom <dua5410773@naver.com>
2024-01-30 16:24:35 +0900

    refactor(dto): MemberJoinDto에서 skills 필드 타입 및 구조 변경
    이 커밋에서는 'MemberJoinDto' 클래스의 'skills' 필드에 대한 중요한 변경이 이루어졌습니다. 주요 변경 사항은 다음과 같습니다.
    
    1. 필드 타입 변경
       - 기존에 Set<Skill> 타입이었던 'skills' 필드를 Skill 단일 타입으로 변경
       - 이는 사용자가 가입 시 하나의 기술만을 선택하도록 요구 사항이 변경되었음을 반영
    
    2. 관련 필드 제거
       - 'ProfileImgUrl' 필드가 제거되었습니다.
       - 이 변경은 사용자의 프로필 이미지 URL을 가입 단계에서 더 이상 요구하지 않음을 의미

--- src/main/java/sidepair/member/configuration/dto/MemberJoinDto.java
+++ src/main/java/sidepair/member/configuration/dto/MemberJoinDto.java
@@ -1,19 +1,16 @@
 package sidepair.member.configuration.dto;
 
-import java.util.Set;
+import sidepair.member.domain.Skill;
 import sidepair.member.domain.vo.Email;
 import sidepair.member.domain.vo.Nickname;
 import sidepair.member.domain.vo.Password;
-import sidepair.member.domain.vo.ProfileImgUrl;
-import sidepair.member.domain.vo.Skill;
 
 public record MemberJoinDto(
 
         Email email,
         Password password,
         Nickname nickname,
-        ProfileImgUrl profileImgUrl,
-        Set<Skill> skills
+        Skill skills
 
 ) {
 }
