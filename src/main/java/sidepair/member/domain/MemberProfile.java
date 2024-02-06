868a92f8faca110efd3ceb4a3f45071d74174bf2
donghoonyeom <dua5410773@naver.com>
2024-02-06 16:48:00 +0900

    refactor(domain): MemberProfile 구조 단순화 및 skills 중심으로 변경
    이번 커밋에서는 `MemberProfile` 엔티티의 구조를 단순화하고, 기술 스택(skills)에 중점을 두었습니다. 주요 변경 내용은 다음과 같습니다:
    
    1. 'nickname' 및 'email' 필드 제거
       - 기존에는 `MemberProfile` 엔티티에 사용자의 'nickname'과 'email' 정보를 포함하고 있었지만, 이번 변경을 통해 해당 필드들을 제거했습니다.
       - 이는 `MemberProfile`의 주요 책임을 기술 스택에 집중시키기 위한 결정입니다.
    
    2. 생성자 변경
       - 변경 전에는 'nickname'과 'email'을 매개변수로 받는 생성자가 있었으나, 변경 후에는 'skills'만을 매개변수로 받는 생성자로 수정되었습니다.
       - 이로 인해 `MemberProfile` 인스턴스를 생성할 때 기술 스택 정보만 필요하게 됩니다.

--- src/main/java/sidepair/member/domain/MemberProfile.java
+++ src/main/java/sidepair/member/domain/MemberProfile.java
@@ -6,32 +6,18 @@ import jakarta.persistence.Entity;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import sidepair.global.domain.BaseUpdatedTimeEntity;
-import sidepair.member.domain.vo.Skill;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberProfile extends BaseUpdatedTimeEntity {
-    @Column(length = 10, nullable = false)
-    private String nickname;
-
-    @Column(length = 100, nullable = false)
-    private String email;
 
     @Column(nullable = false)
     private Skill skills;
 
-    public MemberProfile(final String nickname, final String email) {
-        this.nickname = nickname;
-        this.email = email;
+    public MemberProfile(Skill skills) {
+        this.skills = skills;
     }
 
-    public String getNickname() {
-        return nickname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
 
     public Skill getSkills() {
         return skills;
