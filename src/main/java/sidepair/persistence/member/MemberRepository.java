ac0691dc5bbf1938c570c3c0eb7c9ff49cca0d31
donghoonyeom <dua5410773@naver.com>
2024-02-06 16:51:50 +0900

    refactor(persistence): MemberRepository에 MemberQueryRepository 통합
    이번 커밋에서는 `MemberRepository` 인터페이스의 구조를 개선했습니다. 주요 변경 사항은 다음과 같습니다:
    
    - `MemberRepository`가 `MemberQueryRepository` 인터페이스를 확장하도록 변경하였습니다. 이를 통해 `MemberRepository`를 사용하는 곳에서 별도의 커스텀 쿼리 인터페이스를 주입받지 않고도, `MemberQueryRepository`에서 제공하는 메서드를 직접 사용할 수 있게 되었습니다.

--- src/main/java/sidepair/persistence/member/MemberRepository.java
+++ src/main/java/sidepair/persistence/member/MemberRepository.java
@@ -1,13 +1,15 @@
-package sidepair.member.domain;
+package sidepair.persistence.member;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import sidepair.member.domain.Member;
 import sidepair.member.domain.vo.Email;
 import sidepair.member.domain.vo.Nickname;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberQueryRepository {
 
     Optional<Member> findByNickname(Nickname nickname);
 
     Optional<Member> findByEmail(final Email email);
+
 }
