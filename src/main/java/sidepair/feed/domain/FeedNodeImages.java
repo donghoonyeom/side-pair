3b6c068ce6424bf2487994666a53bffddf78b080
donghoonyeom <dua5410773@naver.com>
2024-02-29 15:01:06 +0900

    feat(feed): FeedNodeImages 컬렉션 엔티티 추가로 이미지 제한 기능 구현
    피드 노드의 이미지를 관리하는 `FeedNodeImages` 컬렉션 엔티티를 추가하여, 하나의 노드에 최대 2개의 이미지만을 허용하는 비즈니스 규칙을 구현했습니다. 이 엔티티의 주요 기능은 다음과 같습니다:
    
    - `add(FeedNodeImage feedNodeImage)`: 새로운 `FeedNodeImage` 인스턴스를 컬렉션에 추가합니다. 이 때, 이미지의 최대 개수 제한을 초과하지 않는지 검증합니다.
    - `addAll(FeedNodeImages images)`: 다른 `FeedNodeImages` 인스턴스의 모든 이미지를 현재 컬렉션에 추가합니다. 추가 후에도 이미지의 최대 개수 제한을 초과하지 않는지 검증합니다.
    - `validateSize(List<FeedNodeImage> images)`: 주어진 이미지 리스트가 설정된 최대 개수를 초과하는지 검증합니다. 초과할 경우, 예외를 발생시킵니다.
    
    이 변경을 통해 시스템은 피드 노드에 대한 이미지 추가 시, 정해진 규칙을 준수하도록 보장합니다. 이는 데이터의 일관성을 유지하고 사용자 경험을 개선하는 데 기여합니다.

---
+++ src/main/java/sidepair/feed/domain/FeedNodeImages.java
@@ -0,0 +1,46 @@
+package sidepair.feed.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.NoArgsConstructor;
+import sidepair.feed.exception.FeedException;
+
+@Embeddable
+@NoArgsConstructor
+public class FeedNodeImages {
+    private static final int MAX_SIZE = 2;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "feed_node_id", nullable = false, updatable = false)
+    private final List<FeedNodeImage> values = new ArrayList<>();
+
+    public FeedNodeImages(final List<FeedNodeImage> images) {
+        validateSize(images);
+        this.values.addAll(new ArrayList<>(images));
+    }
+
+    private void validateSize(final List<FeedNodeImage> images) {
+        if (images.size() > MAX_SIZE) {
+            throw new FeedException("하나의 노드에 사진은 최대 2개까지 가능합니다.");
+        }
+    }
+
+    public void addAll(final FeedNodeImages images) {
+        this.values.addAll(new ArrayList<>(images.values));
+        validateSize(this.values);
+    }
+
+    public void add(final FeedNodeImage feedNodeImage) {
+        this.values.add(feedNodeImage);
+        validateSize(this.values);
+    }
+
+    public List<FeedNodeImage> getValues() {
+        return new ArrayList<>(values);
+    }
+}
