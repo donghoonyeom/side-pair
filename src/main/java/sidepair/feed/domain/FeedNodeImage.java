9638bdc52169d0d2777ab6c62b9e644e95ba39cf
donghoonyeom <dua5410773@naver.com>
2024-02-29 14:59:53 +0900

    feat(feed): FeedNodeImage 엔티티 추가로 노드 이미지 관리 기능 구현
    피드 노드에 포함될 이미지를 관리하는 `FeedNodeImage` 엔티티를 추가했습니다. 이 엔티티는 피드의 개별 노드에 속한 이미지 파일의 정보를 저장하고 관리하는 데 사용됩니다. 주요 특징은 다음과 같습니다:
    
    - `originalFileName`: 업로드된 파일의 원본 파일명을 저장합니다.
    - `serverFilePath`: 서버 내 저장된 파일의 경로를 저장합니다. 이를 통해 이미지 파일에 접근할 수 있습니다.
    - `imageContentType`: 이미지의 컨텐츠 타입을 나타내는 `ImageContentType` 열거형을 사용하여 이미지의 타입(예: JPEG, PNG 등)을 저장합니다.

---
+++ src/main/java/sidepair/feed/domain/FeedNodeImage.java
@@ -0,0 +1,41 @@
+package sidepair.feed.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import sidepair.global.domain.BaseEntity;
+import sidepair.global.domain.ImageContentType;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FeedNodeImage extends BaseEntity {
+    @Column(length = 100, nullable = false)
+    private String originalFileName;
+
+    @Column(nullable = false)
+    private String serverFilePath;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(length = 10, nullable = false)
+    private ImageContentType imageContentType;
+
+    public FeedNodeImage(final String originalFileName, final String serverFilePath,
+                            final ImageContentType imageContentType) {
+        this(null, originalFileName, serverFilePath, imageContentType);
+    }
+
+    public FeedNodeImage(final Long id, final String originalFileName, final String serverFilePath,
+                            final ImageContentType imageContentType) {
+        this.id = id;
+        this.originalFileName = originalFileName;
+        this.serverFilePath = serverFilePath;
+        this.imageContentType = imageContentType;
+    }
+
+    public String getServerFilePath() {
+        return serverFilePath;
+    }
+}
