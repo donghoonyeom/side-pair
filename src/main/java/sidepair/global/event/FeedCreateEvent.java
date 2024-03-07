49d115d7552ccf6a95a48e34595af467e66218de
donghoonyeom <dua5410773@naver.com>
2024-03-07 19:07:48 +0900

    feat(feed-event-listener): 피드 생성 이벤트 리스너 구현
    FeedCreateEventListener 클래스를 추가하여 피드 생성 이벤트를 처리하는 로직을 구현합니다. 이 클래스는 피드 생성 후 발생하는 `FeedCreateEvent`를 비동기적으로 처리하여, 피드의 내용과 관련된 이미지 파일을 저장하는 기능을 수행합니다.
    
    이벤트 처리 과정에서는 다음 작업을 수행합니다:
    - 피드 생성 이벤트를 받아 해당 이벤트 내에 포함된 피드와 피드 노드 정보를 사용하여 이미지 파일을 저장합니다.
    - 피드 내의 마지막 컨텐츠를 찾아, 해당 컨텐츠에 속한 피드 노드의 이미지들을 저장하는 과정을 수행합니다.
    - 파일 정보를 기반으로 서버에 이미지를 저장하고, 저장된 경로 정보를 피드 노드 이미지에 추가합니다.
    
    이 과정은 피드 생성 과정에서의 사용자 경험을 향상시키고, 피드 내용에 대한 시각적 정보를 효율적으로 관리하기 위한 기반을 마련합니다. 이 클래스의 구현을 통해 피드 관련 작업의 비즈니스 로직 처리가 강화되며, 애플리케이션의 기능 확장성에 기여합니다.

---
+++ src/main/java/sidepair/global/event/FeedCreateEvent.java
@@ -0,0 +1,11 @@
+//package sidepair.global.event;
+//
+//import sidepair.feed.configuration.dto.FeedSaveDto;
+//import sidepair.feed.domain.Feed;
+//
+//public record FeedCreateEvent(
+//        Feed feed,
+//        FeedSaveDto feedSaveDto
+//) {
+//
+//}
