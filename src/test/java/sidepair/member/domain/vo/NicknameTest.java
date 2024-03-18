b4dc5e4e35ba209a3ec6b18291100ee5fda32a5a
donghoonyeom <dua5410773@naver.com>
2024-03-18 19:34:08 +0900

    test(member): 회원 관련 도메인 유효성 검사 테스트 추가
    - 이메일, 닉네임, 비밀번호 도메인 객체의 유효성 검증 로직에 대한 테스트 코드를 추가함.
    - 정상적인 조건과 예외를 던져야 하는 조건들을 다양한 시나리오로 분리하여 테스트를 진행함.
    - 유효성 검사 실패 시 `MemberException`을 던지는지 확인하는 테스트를 포함함.

---
+++ src/test/java/sidepair/member/domain/vo/NicknameTest.java
@@ -0,0 +1,30 @@
+package sidepair.member.domain.vo;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import sidepair.member.exception.MemberException;
+
+class NicknameTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ab", "abcdefgh", "AB", "ABCDEFGH", "~!", "~!@#$%^&"})
+    void 정상적으로_닉네임을_생성한다(final String nickname) {
+        //given
+        //when
+        //then
+        assertDoesNotThrow(() -> new Nickname(nickname));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abcdefghi", "123456789012345678901"})
+    void 닉네임_길이가_틀릴_경우_예외를_던진다(final String nickname) {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> new Nickname(nickname))
+                .isInstanceOf(MemberException.class);
+    }
+}
