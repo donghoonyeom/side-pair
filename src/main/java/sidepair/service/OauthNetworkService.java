8fa0c13c4ebeec67986c0ebb9a7556260335124b
donghoonyeom <dua5410773@naver.com>
2024-03-25 22:20:16 +0900

    feat(oauth): 외부 OAuth 서비스와의 통신을 위한 인터페이스 추가
    외부 OAuth 제공자로부터 토큰 및 회원 정보를 요청하는 기능을 담당하는 OauthNetworkService 인터페이스를 추가합니다. 이 인터페이스는 외부 OAuth 서비스와의 네트워크 통신을 추상화하여, 토큰 요청과 회원 정보 요청을 위한 메서드를 정의합니다. 각 메서드는 범용적으로 사용될 수 있도록 제네릭 타입을 지원하며, 요청에 필요한 파라미터는 메서드 인자를 통해 전달됩니다. 이를 통해 다양한 OAuth 제공자와의 통신을 유연하게 처리할 수 있습니다.

---
+++ src/main/java/sidepair/service/OauthNetworkService.java
@@ -0,0 +1,11 @@
+package sidepair.service;
+
+import java.util.Map;
+import org.springframework.http.ResponseEntity;
+
+public interface OauthNetworkService {
+
+    <T> ResponseEntity<T> requestToken(final Class<T> clazz, final Map<String, String> queryParams);
+
+    <T> ResponseEntity<T> requestMemberInfo(final Class<T> clazz, final Map<String, String> headers);
+}
