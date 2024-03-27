1783ce13a86e2faf0550dd604ab0332d412a9aee
donghoonyeom <dua5410773@naver.com>
2024-03-27 22:00:02 +0900

    refactor(member): 공개 회원 정보 응답 구조 개선
    공개적으로 제공되는 회원 정보 응답 구조인 `MemberInformationForPublicResponse`를 수정하였습니다. 기존의 닉네임, 프로필 이미지 URL, 기술 목록으로 이루어진 응답에서 포지션 정보를 추가하고, 기술 목록을 `List<MemberSkillResponse>`로 변경하여 더 상세한 정보를 제공합니다. 이 변경으로 사용자의 포지션과 기술에 대한 정보를 더욱 명확하게 파악할 수 있게 되었습니다.

--- src/main/java/sidepair/member/configuration/response/MemberInformationForPublicResponse.java
+++ src/main/java/sidepair/member/configuration/response/MemberInformationForPublicResponse.java
@@ -5,7 +5,8 @@ import java.util.List;
 public record MemberInformationForPublicResponse(
         String nickname,
         String profileImageUrl,
-        List<String> skills
+        String position,
+        List<MemberSkillResponse> skills
 ) {
 
 }
