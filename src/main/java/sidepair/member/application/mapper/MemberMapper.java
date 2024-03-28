538cc501194f485fbbb8b395fdf1bf8a5500c95c
donghoonyeom <dua5410773@naver.com>
2024-03-28 20:23:15 +0900

    feat(MemberMapper): 멤버 매핑 로직에 포지션 및 멤버 스킬 정보 추가
    멤버의 포지션 정보와 스킬 정보를 처리하기 위해 MemberMapper 클래스를 수정하였습니다. 이제 멤버 가입 시 포지션 정보와 멤버 스킬을 저장할 수 있으며, 멤버 정보 조회 시에도 이를 반영하여 반환합니다. 이 변경을 통해 사용자 경험을 개선하고, 멤버의 정보를 보다 상세히 관리할 수 있게 되었습니다.

--- src/main/java/sidepair/member/application/mapper/MemberMapper.java
+++ src/main/java/sidepair/member/application/mapper/MemberMapper.java
@@ -1,18 +1,24 @@
 package sidepair.member.application.mapper;
 
 
+import java.util.Collections;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import sidepair.member.configuration.dto.MemberInformationDto;
 import sidepair.member.configuration.dto.MemberInformationForPublicDto;
+import sidepair.member.configuration.dto.MemberSkillDto;
+import sidepair.member.configuration.dto.MemberSkillSaveDto;
 import sidepair.member.configuration.request.MemberJoinRequest;
 import sidepair.member.configuration.dto.MemberJoinDto;
+import sidepair.member.configuration.request.MemberSkillSaveRequest;
 import sidepair.member.configuration.response.MemberInformationForPublicResponse;
 import sidepair.member.configuration.response.MemberInformationResponse;
+import sidepair.member.configuration.response.MemberSkillResponse;
+import sidepair.member.domain.Position;
 import sidepair.member.domain.vo.Email;
 import sidepair.member.domain.vo.Nickname;
 import sidepair.member.domain.vo.Password;
-import sidepair.member.domain.Skill;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberMapper {
@@ -20,20 +26,42 @@ public class MemberMapper {
         final Email email = new Email(request.email());
         final Password password = new Password(request.password());
         final Nickname nickname = new Nickname(request.nickname());
-        final Skill skills = Skill.valueOf(request.skills().name());
-        return new MemberJoinDto(email, password, nickname, skills);
+        final Position position = Position.valueOf(request.positionType().name());
+        final List<MemberSkillSaveDto> skills = convertToMemberSkillSaveDtos(request);
+        return new MemberJoinDto(email, password, nickname, position, skills);
     }
 
     public static MemberInformationResponse convertToMemberInformationResponse(
             final MemberInformationDto memberInformationDto) {
         return new MemberInformationResponse(memberInformationDto.id(), memberInformationDto.nickname(),
-                memberInformationDto.profileImageUrl(), memberInformationDto.skills(), memberInformationDto.email());
+                memberInformationDto.profileImageUrl(), memberInformationDto.position(),
+                convertMemberSkillResponses(memberInformationDto.skills()), memberInformationDto.email());
     }
 
     public static MemberInformationForPublicResponse convertToMemberInformationForPublicResponse(
             final MemberInformationForPublicDto memberInformationForPublicDto) {
         return new MemberInformationForPublicResponse(memberInformationForPublicDto.nickname(),
-                memberInformationForPublicDto.profileImageUrl(),
-                memberInformationForPublicDto.skills());
+                memberInformationForPublicDto.profileImageUrl(), memberInformationForPublicDto.position(),
+                convertMemberSkillResponses(memberInformationForPublicDto.skills()));
+    }
+
+    private static List<MemberSkillResponse> convertMemberSkillResponses(final List<MemberSkillDto> memberSkillDtos) {
+        return memberSkillDtos.stream()
+                .map(tag -> new MemberSkillResponse(tag.id(), tag.name()))
+                .toList();
+    }
+
+    private static MemberSkillSaveDto convertToMemberSkillSaveDto(final MemberSkillSaveRequest request) {
+        return new MemberSkillSaveDto(request.name());
+    }
+
+    private static List<MemberSkillSaveDto> convertToMemberSkillSaveDtos(final MemberJoinRequest request) {
+        if (request.skills() == null) {
+            return Collections.emptyList();
+        }
+        return request.skills()
+                .stream()
+                .map(MemberMapper::convertToMemberSkillSaveDto)
+                .toList();
     }
 }
