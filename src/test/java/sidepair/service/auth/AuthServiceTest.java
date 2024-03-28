e0f01a41c2338c996414ad81cd4b70788400e3ad
donghoonyeom <dua5410773@naver.com>
2024-03-28 21:07:27 +0900

    test(service): AuthService 로그인 및 토큰 재발행 기능에 대한 단위 테스트 추가
    AuthService의 로그인 및 토큰 재발행 기능을 검증하기 위한 다양한 시나리오에 대한 단위 테스트를 추가하였습니다. 주요 검증 포인트는 다음과 같습니다:
    
    1. 정상적인 로그인 프로세스가 성공적으로 수행되는지 확인합니다.
    2. 존재하지 않는 이메일로 로그인을 시도할 경우 예외를 정상적으로 던지는지 검증합니다.
    3. 일치하지 않는 비밀번호로 로그인을 시도할 경우 예외가 발생하는지 검증합니다.
    4. 리프레시 토큰을 이용한 액세스 토큰 재발행이 정상적으로 이루어지는지 확인합니다.
    5. 유효하지 않거나 만료된 리프레시 토큰으로 토큰 재발행을 시도할 경우 예외가 발생하는지 검증합니다.
    6. 리프레시 토큰으로 조회한 회원 정보가 존재하지 않을 경우 예외를 던지는지 검증합니다.
    
    이러한 테스트를 통해 AuthService의 핵심 기능인 사용자 인증 및 토큰 관리 로직의 안정성을 보장할 수 있습니다. 또한, 예외 처리 로직의 정확성을 검증하여 사용자 인증 과정에서의 다양한 오류 상황을 효과적으로 관리할 수 있습니다.

---
+++ src/test/java/sidepair/service/auth/AuthServiceTest.java
@@ -0,0 +1,183 @@
+package sidepair.service.auth;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sidepair.member.domain.EncryptedPassword;
+import sidepair.member.domain.Member;
+import sidepair.member.domain.MemberProfile;
+import sidepair.member.domain.MemberSkill;
+import sidepair.member.domain.MemberSkills;
+import sidepair.member.domain.Position;
+import sidepair.member.domain.vo.Email;
+import sidepair.member.domain.vo.Nickname;
+import sidepair.member.domain.vo.Password;
+import sidepair.member.domain.vo.SkillName;
+import sidepair.persistence.auth.RefreshTokenRepository;
+import sidepair.persistence.member.MemberRepository;
+import sidepair.service.dto.auth.request.LoginRequest;
+import sidepair.service.dto.auth.request.ReissueTokenRequest;
+import sidepair.service.dto.auth.response.AuthenticationResponse;
+import sidepair.service.exception.AuthenticationException;
+
+@ExtendWith(MockitoExtension.class)
+class AuthServiceTest {
+
+    private static Member member;
+
+    @Mock
+    private TokenProvider tokenProvider;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private RefreshTokenRepository refreshTokenRepository;
+
+    @InjectMocks
+    private AuthService authService;
+
+    @BeforeAll
+    static void setUp() {
+        final Email email = new Email("user@example.com");
+        final Password password = new Password("password1!");
+        final EncryptedPassword encryptedPassword = new EncryptedPassword(password);
+        final Nickname nickname = new Nickname("nickname");
+        final MemberSkills skills= new MemberSkills(
+                List.of(new MemberSkill(1L, new SkillName("Java"))));
+        final MemberProfile memberProfile = new MemberProfile(Position.BACKEND);
+        member = new Member(email, encryptedPassword, nickname, null, memberProfile, skills);
+    }
+
+    @Test
+    void 정상적으로_로그인을_한다() {
+        //given
+        final LoginRequest loginRequest = new LoginRequest("user@example.com", "password1!");
+        final String accessToken = "accessToken";
+        final String refreshToken = "refreshToken";
+
+        given(memberRepository.findByEmail(any()))
+                .willReturn(Optional.of(member));
+        given(tokenProvider.createAccessToken(any(), any()))
+                .willReturn(accessToken);
+        given(tokenProvider.createRefreshToken(any(), any()))
+                .willReturn(refreshToken);
+
+        //when
+        final AuthenticationResponse authenticationResponse = authService.login(loginRequest);
+
+        //then
+        assertThat(authenticationResponse).isEqualTo(
+                new AuthenticationResponse(refreshToken, accessToken));
+    }
+
+    @Test
+    void 존재하지_않는_이메일로_로그인_하는_경우_예외를_던진다() {
+        //given
+        final LoginRequest loginRequest = new LoginRequest("user@example.com", "password1!");
+        given(memberRepository.findByEmail(any()))
+                .willReturn(Optional.empty());
+
+        //when
+        //then
+        assertThatThrownBy(() -> authService.login(loginRequest))
+                .isInstanceOf(AuthenticationException.class);
+    }
+
+    @Test
+    void 일치하지_않는_비밀번호로_로그인_하는_경우_예외를_던진다() {
+        //given
+        final LoginRequest loginRequest = new LoginRequest("user@example.com", "wrongpassword1!");
+        given(memberRepository.findByEmail(any()))
+                .willReturn(Optional.of(member));
+
+        //when
+        //then
+        assertThatThrownBy(() -> authService.login(loginRequest))
+                .isInstanceOf(AuthenticationException.class);
+    }
+
+    @Test
+    void 정상적으로_토큰을_재발행한다() {
+        //given
+        final String accessToken = "accessToken";
+        final String refreshToken = "refreshToken";
+        final ReissueTokenRequest reissueTokenRequest = new ReissueTokenRequest("refreshToken");
+
+        given(tokenProvider.isValidToken(any()))
+                .willReturn(true);
+        given(tokenProvider.createAccessToken(any(), any()))
+                .willReturn(accessToken);
+        given(tokenProvider.createRefreshToken(any(), any()))
+                .willReturn(refreshToken);
+        given(memberRepository.findByEmail(any()))
+                .willReturn(Optional.of(member));
+        given(refreshTokenRepository.findMemberEmailByRefreshToken(any()))
+                .willReturn(Optional.of(member.getEmail().getValue()));
+
+        //when
+        final AuthenticationResponse authenticationResponse = authService.reissueToken(reissueTokenRequest);
+
+        //then
+        assertThat(authenticationResponse).isEqualTo(new AuthenticationResponse(refreshToken, accessToken));
+    }
+
+    @Test
+    void 리프레시_토큰이_유효하지_않을_경우_예외를_던진다() {
+        //given
+        final String refreshToken = "refreshToken";
+        final ReissueTokenRequest reissueTokenRequest = new ReissueTokenRequest(refreshToken);
+        given(tokenProvider.isValidToken(any()))
+                .willReturn(false);
+
+        //when
+        //then
+        assertThatThrownBy(() -> authService.reissueToken(reissueTokenRequest))
+                .isInstanceOf(AuthenticationException.class);
+    }
+
+    @Test
+    void 리프레시_토큰이_만료_됐을_경우_예외를_던진다() {
+        //given
+        final String refreshToken = "refreshToken";
+        final ReissueTokenRequest reissueTokenRequest = new ReissueTokenRequest(refreshToken);
+        given(tokenProvider.isValidToken(any()))
+                .willReturn(true);
+        given(refreshTokenRepository.findMemberEmailByRefreshToken(any()))
+                .willReturn(Optional.empty());
+
+        //when
+        //then
+        assertThatThrownBy(() -> authService.reissueToken(reissueTokenRequest))
+                .isInstanceOf(AuthenticationException.class);
+    }
+
+    @Test
+    void 리프레시_토큰으로_조회한_회원이_존재하지_않는_경우_예외를_던진다() {
+        //given
+        final String refreshToken = "refreshToken";
+        final ReissueTokenRequest reissueTokenRequest = new ReissueTokenRequest(refreshToken);
+        given(tokenProvider.isValidToken(any()))
+                .willReturn(true);
+        given(refreshTokenRepository.findMemberEmailByRefreshToken(any()))
+                .willReturn(Optional.of(member.getEmail().getValue()));
+        given(memberRepository.findByEmail(any()))
+                .willReturn(Optional.empty());
+
+        //when
+        //then
+        assertThatThrownBy(() -> authService.reissueToken(reissueTokenRequest))
+                .isInstanceOf(AuthenticationException.class);
+    }
+}
