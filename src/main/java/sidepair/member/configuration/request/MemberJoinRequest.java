85b76039eba82d3d0802539283d6e1c6e27bb535
donghoonyeom <dua5410773@naver.com>
2024-03-28 20:20:37 +0900

    refactor(member): 회원가입 요청 DTO의 포지션 타입 경로 수정
    회원가입 요청 처리를 위한 DTO에서 사용되는 포지션 타입의 패키지 경로를 수정했습니다. 이는 시스템의 모듈화를 개선하고, 의존성 관리를 더욱 명확하게 하기 위한 변경입니다. 해당 변경으로 인해 회원가입 시 포지션 타입 관련 처리가 더욱 안정적으로 이루어질 것으로 기대됩니다.

--- src/main/java/sidepair/member/configuration/request/MemberJoinRequest.java
+++ src/main/java/sidepair/member/configuration/request/MemberJoinRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
-import sidepair.member.domain.PositionType;
+import sidepair.service.dto.mamber.request.PositionType;
 
 public record MemberJoinRequest(
         @NotBlank(message = "이메일은 빈 값일 수 없습니다.")
@@ -15,7 +15,7 @@ public record MemberJoinRequest(
         @NotBlank(message = "비밀번호는 빈 값일 수 없습니다.")
         String password,
 
-        @NotBlank(message = "닉네임 빈 값일 수 없습니다.")
+        @NotBlank(message = "닉네임은 빈 값일 수 없습니다.")
         String nickname,
 
         @NotNull(message = "포지션은 빈 값일 수 없습니다.")
