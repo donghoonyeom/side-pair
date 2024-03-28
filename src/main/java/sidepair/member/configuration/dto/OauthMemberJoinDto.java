54c3c752ab0211eb92f9cb29a1f3b3cb02faab65
donghoonyeom <dua5410773@naver.com>
2024-03-28 20:24:13 +0900

    feat(OauthMemberJoinDto): OAuth 회원 가입 DTO 포지션 타입 필드 추가
    OAuth를 통한 회원 가입 시 필요한 정보를 담고 있는 OauthMemberJoinDto에 포지션 타입을 나타내는 필드를 추가함.
    이를 통해 OAuth를 이용한 회원 가입 시 사용자의 포지션 정보도 함께 수집할 수 있게 되어, 사용자의 역할 및 관심 분야를
    더 정확히 반영할 수 있게 됨. 기존에는 사용자의 OAuth ID, 이메일, 닉네임만을 수집했으나, 이번 변경으로 포지션 타입도
    함께 수집하게 되어 사용자 경험 개선 및 서비스의 정확도를 높일 것으로 기대됨.
    
    - OauthMemberJoinDto 클래스에 PositionType 타입의 position 필드 추가

--- src/main/java/sidepair/member/configuration/dto/OauthMemberJoinDto.java
+++ src/main/java/sidepair/member/configuration/dto/OauthMemberJoinDto.java
@@ -1,8 +1,11 @@
 package sidepair.member.configuration.dto;
 
+import sidepair.service.dto.mamber.request.PositionType;
+
 public record OauthMemberJoinDto(
         String oauthId,
         String email,
+        PositionType position,
         String nickname
 ) {
 }
