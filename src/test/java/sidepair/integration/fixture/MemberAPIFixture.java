5df87d0a72710e581cf349b2f210ff52935d9367
donghoonyeom <dua5410773@naver.com>
2024-04-01 20:21:41 +0900

    feat(member): 회원 관련 API 테스트 픽스처 추가
    이 커밋은 회원가입, 회원 정보 조회 등 회원 관련 기능을 테스트하기 위한 픽스처 클래스인 MemberAPIFixture를 추가합니다. 이를 통해 다음과 같은 기능을 테스트할 수 있습니다.
    
    - 회원가입: 요청을 받는 회원가입과 기본 회원가입 메소드를 통해, 테스트 환경에서 필요한 회원 가입 과정을 자동화합니다.
    - 회원 정보 조회: 사용자 자신의 정보 조회와 특정 사용자의 정보 조회 요청을 수행하는 메소드를 통해, 테스트 시 회원 정보의 정확성을 검증할 수 있습니다.
    - 사용자 추가 및 토큰 조회: 특정 정보(이메일, 닉네임)로 사용자를 추가하고 로그인하여 토큰을 조회하는 메소드를 통해, 인증 관련 테스트를 용이하게 진행할 수 있습니다.
    
    이 클래스는 테스트 코드의 중복을 줄이고, 테스트의 가독성과 유지보수성을 높이기 위해 설계되었습니다.

---
+++ src/test/java/sidepair/integration/fixture/MemberAPIFixture.java
@@ -0,0 +1,79 @@
+package sidepair.integration.fixture;
+
+import static io.restassured.RestAssured.given;
+import static sidepair.integration.fixture.AuthenticationAPIFixture.로그인;
+import static sidepair.integration.fixture.CommonFixture.API_PREFIX;
+import static sidepair.integration.fixture.CommonFixture.AUTHORIZATION;
+import static sidepair.integration.fixture.CommonFixture.BEARER_TOKEN_FORMAT;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.List;
+import org.springframework.http.MediaType;
+import sidepair.service.dto.mamber.request.MemberJoinRequest;
+import sidepair.service.dto.mamber.request.MemberSkillSaveRequest;
+import sidepair.service.dto.auth.request.LoginRequest;
+import sidepair.service.dto.mamber.request.PositionType;
+
+public class MemberAPIFixture {
+
+    public static final String DEFAULT_EMAIL = "test@gmail.com";
+    public static final String DEFAULT_PASSWORD = "password1!";
+    public static final String DEFAULT_NICKNAME = "nickname";
+    public static final PositionType DEFAULT_POSITION_TYPE = PositionType.BACKEND;
+    public static final List<MemberSkillSaveRequest> DEFAULT_SKILLS =
+            List.of(new MemberSkillSaveRequest("Java"), new MemberSkillSaveRequest("Spring"));
+
+    public static ExtractableResponse<Response> 요청을_받는_회원가입(final MemberJoinRequest 회원가입_요청) {
+        return given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .body(회원가입_요청)
+                .post(API_PREFIX + "/members/join")
+                .then()
+                .log().all()
+                .extract();
+    }
+
+    public static Long 회원가입(final MemberJoinRequest 회원가입_요청) {
+        final Response 회원가입_응답 = 요청을_받는_회원가입(회원가입_요청).response();
+        final String Location_헤더 = 회원가입_응답.header("Location");
+        return Long.parseLong(Location_헤더.substring(13));
+    }
+
+    public static Long 기본_회원가입() {
+        final MemberJoinRequest 회원가입_요청 = new MemberJoinRequest(DEFAULT_EMAIL, DEFAULT_PASSWORD, DEFAULT_NICKNAME,
+                DEFAULT_POSITION_TYPE, DEFAULT_SKILLS);
+        return 회원가입(회원가입_요청);
+    }
+
+    public static ExtractableResponse<Response> 요청을_받는_사용자_자신의_정보_조회_요청(final String 액세스_토큰) {
+        return given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .header(AUTHORIZATION, 액세스_토큰)
+                .get(API_PREFIX + "/members/me")
+                .then()
+                .log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 요청을_받는_특정_사용자의_정보_조회(final String 액세스_토큰, final Long 조회할_회원_아이디) {
+        return given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .header(AUTHORIZATION, 액세스_토큰)
+                .get(API_PREFIX + "/members/{memberId}", 조회할_회원_아이디)
+                .then()
+                .log().all()
+                .extract();
+    }
+
+    public static String 사용자를_추가하고_토큰을_조회한다(final String 이메일, final String 닉네임) {
+        final MemberJoinRequest 팔로워_회원_가입_요청 = new MemberJoinRequest(이메일, "paswword2@",
+                닉네임, PositionType.BACKEND, DEFAULT_SKILLS);
+        final LoginRequest 팔로워_로그인_요청 = new LoginRequest(팔로워_회원_가입_요청.email(), 팔로워_회원_가입_요청.password());
+        회원가입(팔로워_회원_가입_요청);
+        return String.format(BEARER_TOKEN_FORMAT, 로그인(팔로워_로그인_요청).accessToken());
+    }
+}
