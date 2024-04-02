268c2f9c2f369dbeb584356cdced192e288b213a
donghoonyeom <dua5410773@naver.com>
2024-04-03 00:00:45 +0900

    feat(FeedApplicantRepository): 피드 지원자 관리를 위한 JPA 리포지토리 추가
    피드에 지원한 멤버를 관리하기 위한 FeedApplicantRepository 인터페이스를 추가.
    특정 피드와 멤버를 기준으로 지원자 정보를 조회하는 기능을 포함하여, 피드 지원자 관련 데이터 접근을 위한 메서드 제공.
    
    - findByFeedAndMember 메서드를 통해 주어진 피드와 멤버에 해당하는 지원자 정보 조회 기능 구현
    
    이 변경을 통해 애플리케이션에서 피드에 지원한 멤버들을 효율적으로 관리하고, 특정 피드와 멤버 조합에 대한 지원자 존재 여부를 쉽게 확인할 수 있게 됩니다. 리포지토리 계층의 확장성과 유지보수성을 개선하는 데 기여할 것으로 기대됩니다.

---
+++ src/main/java/sidepair/persistence/feed/FeedApplicantRepository.java
@@ -0,0 +1,11 @@
+package sidepair.persistence.feed;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import sidepair.domain.feed.Feed;
+import sidepair.domain.feed.FeedApplicant;
+import sidepair.domain.member.Member;
+
+public interface FeedApplicantRepository extends JpaRepository<FeedApplicant, Long>, FeedApplicantQueryRepository{
+    Optional<FeedApplicant> findByFeedAndMember(final Feed feed, final Member member);
+}
