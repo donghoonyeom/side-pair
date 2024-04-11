b18ed5d94b6424a744771ca1929bb9c149508a42
donghoonyeom <dua5410773@naver.com>
2024-04-11 23:15:54 +0900

    refactor(member-dto): 멤버 DTO 및 응답 객체에 포지션과 스킬 정보 추가
    멤버 관련 DTO와 응답 객체에 대한 리팩토링을 진행하였습니다. 기존에는 멤버의 기본 정보(아이디, 이름, 이미지 URL)만을 포함하던 DTO와 응답 객체에 포지션(position) 정보와 스킬(skills) 리스트를 추가함으로써, 멤버의 전문성과 역량을 보다 상세하게 표현할 수 있도록 개선했습니다.
    
    - `MemberDto`와 `MemberResponse` 객체에 `position` 필드를 추가하여, 멤버의 직무나 역할을 문자열로 나타낼 수 있게 되었습니다. 이를 통해 프로젝트 내에서 멤버들의 역할 분담이나 전문성을 명확히 파악할 수 있게 됩니다.
    
    - `skills` 필드는 `MemberSkillDto`와 `MemberSkillResponse`의 리스트를 포함하도록 추가되었습니다. 각 멤버가 보유하고 있는 스킬을 나타내는 이 리스트를 통해 멤버의 구체적인 기술 역량을 파악할 수 있으며, 프로젝트 팀 구성이나 업무 분배 시 참고할 수 있는 중요한 정보를 제공합니다.
    
    이러한 변경을 통해 멤버 관련 정보를 더욱 포괄적으로 제공함으로써, 사용자들이 팀원을 선택하거나 프로젝트를 기획할 때 보다 많은 정보를 기반으로 의사결정을 할 수 있게 되었습니다. 또한, 멤버들의 전문성과 역량을 강조하여 프로젝트의 성공 가능성을 높이는데 기여할 것으로 기대됩니다.

--- src/main/java/sidepair/service/dto/mamber/MemberDto.java
+++ src/main/java/sidepair/service/dto/mamber/MemberDto.java
@@ -1,8 +1,12 @@
 package sidepair.service.dto.mamber;
 
+import java.util.List;
+
 public record MemberDto(
         long id,
         String name,
-        String imageUrl
+        String imageUrl,
+        String position,
+        List<MemberSkillDto> skills
 ) {
 }
--- src/main/java/sidepair/service/dto/mamber/response/MemberResponse.java
+++ src/main/java/sidepair/service/dto/mamber/response/MemberResponse.java
@@ -1,9 +1,14 @@
 package sidepair.service.dto.mamber.response;
 
+
+import java.util.List;
+
 public record MemberResponse(
         long id,
         String name,
-        String imageUrl
+        String imageUrl,
+        String position,
+        List<MemberSkillResponse> skills
 ) {
 
 }
