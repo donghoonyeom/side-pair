5d4396bab1f5bd84530fa0c311f5864b9b4dc904
donghoonyeom <dua5410773@naver.com>
2024-05-01 19:50:22 +0900

    feat(InitIntegrationTest): 통합 테스트 초기화를 위한 기본 데이터 설정 추가
    InitIntegrationTest 클래스는 통합 테스트 실행 전 필수 데이터를 초기화하는 역할을 합니다. 기본 회원 가입, 로그인, 피드 카테고리 생성 등의 기능을 사전에 수행하여 테스트에 필요한 상태를 구성합니다.
    - 기본 회원 아이디, 로그인 토큰 및 재발행 토큰 초기화
    - 피드 카테고리 생성 및 기본 피드 요청 데이터 구성
    - @BeforeEach 어노테이션을 이용해 각 테스트 실행 전에 사전 데이터 설정을 자동화
    
    이 초기화 클래스는 테스트가 공통된 환경에서 실행될 수 있도록 하여 결과의 일관성을 보장하는 데 중요한 역할을 합니다.

---
+++ src/test/java/sidepair/integration/helper/InitIntegrationTest.java
@@ -0,0 +1,34 @@
+package sidepair.integration.helper;
+
+import static sidepair.integration.fixture.AuthenticationAPIFixture.기본_로그인;
+import static sidepair.integration.fixture.CommonFixture.BEARER_TOKEN_FORMAT;
+import static sidepair.integration.fixture.FeedAPIFixture.카테고리_생성;
+import static sidepair.integration.fixture.MemberAPIFixture.기본_회원가입;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import sidepair.domain.feed.FeedCategory;
+import sidepair.service.dto.feed.requesst.FeedNodeSaveRequest;
+import sidepair.service.dto.feed.requesst.FeedSaveRequest;
+import sidepair.service.dto.feed.requesst.FeedTagSaveRequest;
+
+public class InitIntegrationTest extends IntegrationTest {
+
+    protected static Long 기본_회원_아이디;
+    protected static String 기본_로그인_토큰;
+    protected static String 기본_재발행_토큰;
+    protected static FeedCategory 기본_카테고리;
+    protected static FeedSaveRequest 기본_피드_생성_요청;
+
+    @BeforeEach
+    void init() {
+        기본_회원_아이디 = 기본_회원가입();
+        기본_로그인_토큰 = String.format(BEARER_TOKEN_FORMAT, 기본_로그인().accessToken());
+        기본_재발행_토큰 = 기본_로그인().refreshToken();
+        기본_카테고리 = 카테고리_생성(기본_로그인_토큰, "이커머스");
+        기본_피드_생성_요청 = new FeedSaveRequest(기본_카테고리.getId(), "피드 제목", "피드 소개글",
+                "피드 본문",  30,
+                List.of(new FeedNodeSaveRequest("feed 1st week", "피드 1주차 내용", null)),
+                List.of(new FeedTagSaveRequest("태그1")));
+    }
+}
